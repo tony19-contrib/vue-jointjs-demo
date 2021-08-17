6c06264ea848de0b187daae978096d08a581dbfc
Tony Trinh <tony19@gmail.com>
2021-08-17 08:22:34 -0500

    demo jointjs Vue plugin

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyGraph />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyGraph from './components/MyGraph.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyGraph
   }
 }
 </script>
